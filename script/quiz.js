c0733b842de1777f40c1e8b6f3ab94af06ac7e9b
developeravijit <roysidharth1996@gmail.com>
2026-01-04 19:42:56 +0530

    added section in quiz page

--- script/quiz.js
+++ script/quiz.js
@@ -4,6 +4,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
   window.funnelBtn = () => {
     funnelList.classList.toggle("show");
-    filterBtn.style.color = isOpen ? "#097877" : "";
+    // filterBtn.style.color = isOpen ? "#097877" : "";
   };
+
+  const rangeInput = document.getElementById("range4");
+  const rangeOutput = document.getElementById("rangeValue");
+  rangeOutput.textContent = rangeInput.value + "%";
+
+  rangeInput.addEventListener("input", function () {
+    rangeOutput.textContent = this.value + "%";
+  });
 });
