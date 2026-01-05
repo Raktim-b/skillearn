acd9cd6abf08ec111dd9c091331607d8c949e3a6
developeravijit <roysidharth1996@gmail.com>
2026-01-06 02:56:23 +0530

    added changes header, quiz page, buttons

--- script/CommonScript.js
+++ script/CommonScript.js
@@ -1,19 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   const swiper = new Swiper(".cardSwiper", {
     effect: "cards",
-     loop: true,
+    loop: true,
     grabCursor: true,
     pagination: {
-        el: ".swiper-pagination",
-        type: "fraction",
-        
-      },
-       scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+      el: ".swiper-pagination",
+      type: "fraction",
+    },
+    scrollbar: {
+      el: ".swiper-scrollbar",
+          },
     navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
   });
 });
