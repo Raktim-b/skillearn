0d531f905eab682593c79e0709e174691f83714f
developeravijit <roysidharth1996@gmail.com>
2026-01-09 01:38:12 +0530

    completed dashboard navbar and hdr

--- dashboard/js/main.js
+++ dashboard/js/main.js
@@ -1,12 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Hamberger SideBar Toggle
-  const navBar = document.querySelector(".left-side");
+  const navBar = document.querySelector(".left-sideNav");
+//   const menuButton = document.querySelector(".menu-btn");
+//   const closeBtn = document.querySelector(".close-btn");
+
   window.hamberger = () => {
-    navBar.classList.toggle("click");
+    navBar.classList.add("open");
+
+  };
+
+  window.clickClose = () => {
+    navBar.classList.remove("open");
+
   };
 });
-
-console.log();
-
-
-
