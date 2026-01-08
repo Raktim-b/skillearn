0b850cd96cf335e5799d969fbedb10ebab2b1282
developeravijit <roysidharth1996@gmail.com>
2026-01-09 00:16:41 +0530

    added dashboard landing page

---
+++ dashboard/js/main.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Hamberger SideBar Toggle
+  const navBar = document.querySelector(".left-side");
+  window.hamberger = () => {
+    navBar.classList.toggle("click");
+  };
+});
+
+console.log();
+
+
+
