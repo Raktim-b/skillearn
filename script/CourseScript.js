4886d9d749fb790cb2b50f7eeae3e9fcdb8ae425
Raktim-b <raktimb414@gmail.com>
2026-01-11 16:14:57 +0530

    add to cart complt and linked

--- script/CourseScript.js
+++ script/CourseScript.js
@@ -4,11 +4,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // course title
   const titles = document.querySelectorAll(".title4");
-
+  const cartBtns = document.querySelectorAll(".cart");
   const goToDetails = () => {
     window.location.href = "course-details.html";
   };
-
+  const goCart = () => {
+    window.location.href = "cart.html";
+  };
+  cartBtns.forEach((cartbtn) => {
+    cartbtn.addEventListener("click", goCart);
+  });
   images.forEach((img) => {
     img.addEventListener("click", goToDetails);
   });
